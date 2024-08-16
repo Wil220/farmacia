be3df14f7a2698a4426be5b92de299b6b7608ab4
Wil220 <wilker.silva0714@gmail.com>
2024-08-16 16:13:33 -0300

    CRUD de Categoria implementado

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,28 @@
+import './Home.css';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Bem-vindo à Farmácia</h1>
-    </div>
-  );
-};
+
+function Home() {
+    return (
+        <>
+        <div className="bg-red-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+              <p className='text-xl'>Tudo que precisa esta aqui</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+            </div>
+          </div>
+        </div>
+      
+      </>
+    );
+}
 
 export default Home;
